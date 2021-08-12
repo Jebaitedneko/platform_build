AOSPA/android_build

product: Generate custom audio on AOSPA builds.
 * When building PA, a custom audio set (Ringtones, Alarms, etc.)
   will be used instead of the default AOSP ones.

Change-Id: Ia2f0506bb3d24d8ce9705929e97e227345101e80

target/product/full_base.mk
@@ -43,15 +43,19 @@ PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
 # Additional settings used in all AOSP builds
+ifeq ($(PA_BUILD),)
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.ringtone=Ring_Synth_04.ogg \
     ro.config.notification_sound=pixiedust.ogg
+endif
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
 
 # Get some sounds
+ifeq ($(PA_BUILD),)
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+endif
 
 # Get a list of languages.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)

target/product/handheld_system.mk
@@ -84,5 +84,9 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.carrier=unknown \
+
+ifeq ($(PA_BUILD),)
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.notification_sound=OnTheHunt.ogg \
     ro.config.alarm_alert=Alarm_Classic.ogg
+endif

target/product/mainline.mk
@@ -26,11 +26,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
+ifeq ($(PA_BUILD),)
 $(call inherit-product, frameworks/base/data/sounds/AllAudio.mk)
+endif
 
+ifeq ($(PA_BUILD),)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Ring_Synth_04.ogg \
     ro.com.android.dataroaming=true \
+
+endif
 
 PRODUCT_PACKAGES += \
     PhotoTable \

Jebaitedneko/platform_build

target/product/full_base.mk
@@ -43,15 +43,19 @@ PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
 # Additional settings used in all AOSP builds
+ifeq ($(AOSPA_BUILD),)
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.ringtone=Ring_Synth_04.ogg \
     ro.config.notification_sound=pixiedust.ogg
+endif
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
 
 # Get some sounds
+ifeq ($(AOSPA_BUILD),)
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+endif
 
 # Get a list of languages.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)

target/product/handheld_system.mk
@@ -88,5 +88,9 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.carrier=unknown \
+
+ifeq ($(AOSPA_BUILD),)
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.notification_sound=OnTheHunt.ogg \
     ro.config.alarm_alert=Alarm_Classic.ogg
+endif

target/product/mainline.mk
@@ -26,16 +26,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
+ifeq ($(AOSPA_BUILD),)
 $(call inherit-product, frameworks/base/data/sounds/AllAudio.mk)
+endif
 
+ifeq ($(AOSPA_BUILD),)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Ring_Synth_04.ogg \
     ro.com.android.dataroaming=true \
+
+endif
 
 PRODUCT_PACKAGES += \
     PhotoTable \
     WallpaperPicker \
 
-ifeq ($(AOSPA_BUILD),)
+ifeq ($(AOSAOSPA_BUILD),)
 PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
 endif